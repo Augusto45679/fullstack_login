27a85f190048699d65dcc242bbf7d179593da757
Augusto Berloin <nfunes706@gmail.com>
2025-07-12 16:18:03 -0300

    quick fix login endpoint

--- frontend/src/endpoints/api.js
+++ frontend/src/endpoints/api.js
@@ -4,17 +4,13 @@ const BASE_URL = 'http://127.0.0.1:8000/api';
 const LOGIN_URL = `${BASE_URL}/token/`;
 
 // 1. Login (obtener access y refresh)
-export const loginUser = async (username, password) => {
-  const resp = await fetch(`${BASE_URL}/token/`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ username, password }),
-    credentials: 'include',
-  });
-  if (!resp.ok) throw new Error('Error en login');
-  const data = await resp.json();
-  setTokens(data);
-  return data;
+export const login = async (username, password) => {
+  const response = await axios.post(LOGIN_URL, {
+    username: username,
+    password: password,
+    }, {withCredentials:true
+  })
+  return reponse.data.success;
 };
 
 
